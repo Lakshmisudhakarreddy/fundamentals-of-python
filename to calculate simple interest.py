89e16848478897ad29b5204fc3cb9e2829f8c665
Lakshmisudhakarreddy <61283703+Lakshmisudhakarreddy@users.noreply.github.com>
2020-02-25 13:27:55 +0530

    Add files via upload

---
+++ to calculate simple interest.py
@@ -0,0 +1,6 @@
+#write a program to P,T,R and calculate simple interest
+p=float(input("enter principle amount:"))
+t=float(input("enter time period:"))
+r=float(input("enter rate:"))
+SI=(p*t*r)/100 #simple interest=(P*T*R)/100
+print("simple interest:",SI)
