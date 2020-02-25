4a92c8b6697a1c92ca96b45ded6524c54d3d23d8
Lakshmisudhakarreddy <61283703+Lakshmisudhakarreddy@users.noreply.github.com>
2020-02-25 13:12:10 +0530

    Delete assign1R.py

--- assign1R.py
+++
@@ -1,6 +0,0 @@
-#write a program to P,T,R and calculate compound interest
-p=float(input("enter principle amount:"))
-t=float(input("enter time period:"))
-r=float(input("enter rate:"))
-CI=p*(1+r/100)**t #compound interest=P(1+1/r)^t
-print("compound interest:",CI)
