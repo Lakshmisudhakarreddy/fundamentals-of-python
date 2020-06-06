bb85cf49654329a7282c9b122212112319f7576b
Lakshmisudhakarreddy <61283703+Lakshmisudhakarreddy@users.noreply.github.com>
2020-06-06 08:15:56 +0530

    Add files via upload

---
+++ to calculate x power of y using recursive function.py
@@ -0,0 +1,11 @@
+def power(x,y):
+    if(y==1):
+        return(x)
+    if(y!=1):
+        return(x*power(x,y-1))
+x=int(input('enter x: '))
+y=int(input('enter y: '))
+print("result: ",power(x,y))    
+
+
+
