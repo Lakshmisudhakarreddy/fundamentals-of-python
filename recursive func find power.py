25aea2e5cb6e1f1a34f9763edec0257e826509f3
Lakshmisudhakarreddy <61283703+Lakshmisudhakarreddy@users.noreply.github.com>
2020-06-06 08:08:13 +0530

    Update recursive func find power.py

--- recursive func find power.py
+++ recursive func find power.py
@@ -4,9 +4,6 @@ Created on Sat Jun  6 06:37:15 2020
 
 @author: sudha
 """    
-
-
-
 def power(x,y):
  
     if(y==1):
@@ -15,7 +12,4 @@ def power(x,y):
         return(x*power(x,y-1))
 x=int(input("enter x:"))
 y=int(input("enter y: "))
-print("result:",power(x,y))    
-
-
-input('').split()
+print("result:",power(x,y))   
