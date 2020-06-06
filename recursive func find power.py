17f71f1646ff2c0a2622d965b949b4ffb7862d6a
Lakshmisudhakarreddy <61283703+Lakshmisudhakarreddy@users.noreply.github.com>
2020-06-06 08:10:27 +0530

    Update recursive func find power.py

--- recursive func find power.py
+++ recursive func find power.py
@@ -1,15 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun  6 06:37:15 2020
-
-@author: sudha
-"""    
 def power(x,y):
- 
     if(y==1):
         return(x)
     if(y!=1):
         return(x*power(x,y-1))
 x=int(input("enter x:"))
 y=int(input("enter y: "))
-print("result:",power(x,y))   
+print("result:",power(x,y))
